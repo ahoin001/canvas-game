8517ad3ef3ae404b391c684113cc65e2b24bf23a
Alexander Hoinville <ahoin001@gmail.com>
2019-07-27 10:46:32 -0400

    Revert "removed uneccesary class"
    This reverts commit 6f4df7a737d0186169d858fa108676d53ba73006.

---
+++ src/food.js
@@ -0,0 +1,18 @@
+class Food {
+
+    constructor(xPosition) {
+        this.width = 50;
+        this.height = 50;
+        this.speed = 3;
+
+        // will be random number passed in
+        this.x = xPosition;
+       
+        this.y = 0;
+
+        this.image = new Image();
+        this.image.src = "./images/food.png";
+    }
+
+
+}
